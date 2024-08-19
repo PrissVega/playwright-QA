78ad0c76dfa8fb029bd6df01c33f661b00661146
PrissVega <priscila@blubear.io>
2024-08-19 17:27:17 -0500

    Add test: select card and see user details

--- e2e/E2E_Postventa_process.spec.js
+++ e2e/E2E_Postventa_process.spec.js
@@ -3,11 +3,14 @@ const { test, expect } = require('@playwright/test');
 // Define selectores y datos comunes
 const URL = 'http://localhost:9000/#/';
 const SELECTORS = {
-  usernameInput: 'input[placeholder="Correo electronico"]',
-  passwordInput: 'input[placeholder="password"]',
-  loginButton: 'button:has-text("Iniciar Sesión")',
-  errorMessage: 'div.q-notification__message.col', // Selector actualizado para el mensaje de error
-  sucessfullMessage: 'div.q-notification__message.col',//Selector actualizado para el mensaje satisfactorio
+    usernameInput: 'input[placeholder="Correo electronico"]',
+    passwordInput: 'input[placeholder="password"]',
+    loginButton: 'button:has-text("Iniciar Sesión")',
+    errorMessage: 'div.q-notification__message.col', // Selector actualizado para el mensaje de error
+    successMessage: 'div.q-notification__message.col', // Selector actualizado para el mensaje satisfactorio
+    spanAcceder: 'div.call-card', // Selector para el botón Acceder
+    titleElement: 'h1:has-text("Gestión de Vehículos")', // Selector para el título
+    headerCard: 'div.header-card', // Selector para el div que contiene el h2
 };
 
 // Función para realizar el login
@@ -33,7 +36,7 @@ test.describe('Proceso E2E Postventa', () => {
   test('Login with correct credentials', async ({ page }) => { 
     await login(page, 'wcadena@casabaca.com', 'wcadena@casabaca.com');
     // Verifica que el mensaje de inicio de sesión exitoso que contenga el texto esperado
-    const successfulMessage = await page.locator(SELECTORS.sucessfullMessage).innerText();
+    const successfulMessage = await page.locator(SELECTORS.successMessage).innerText();
     expect(successfulMessage).toContain('Validación de credenciales exitosa..');
   });
 
@@ -69,5 +72,43 @@ test.describe('Proceso E2E Postventa', () => {
     // Verifica que se ha vuelto a la página de inicio
     await page.waitForSelector(SELECTORS.usernameInput, { state: 'visible', timeout: 10000 });
     await expect(page.locator(SELECTORS.usernameInput)).toBeVisible();
+  });
+
+  test('Select card and see user details', async ({ page }) => { 
+    await login(page, 'wcadena@casabaca.com', 'wcadena@casabaca.com');
+    
+    // Espera a que el <span> sea visible y haz clic en él
+    await page.waitForSelector(SELECTORS.spanAcceder, { state: 'visible', timeout: 20000 });
+    await page.locator(SELECTORS.spanAcceder).click();
+
+    // Espera a que el título de la página sea visible y verifica el texto
+    await page.waitForSelector(SELECTORS.titleElement, { state: 'visible', timeout: 10000 });
+    const titleText = await page.locator(SELECTORS.titleElement).innerText();
+    expect(titleText).toContain('Gestión de Vehículos');
+
+    const headerCard = page.locator(`div.header-card:has(h2:has-text("PDS5736"))`);
+    // Verifica que el elemento existe antes de hacer clic
+    await expect(headerCard).toBeVisible();
+    // Haz clic en el <div>
+    await headerCard.click();
+
+    // Selecciona el contenedor principal con la clase title-car
+    const titleCarDiv = page.locator('div.title-car');
+    // Verifica que el contenedor sea visible
+    await expect(titleCarDiv).toBeVisible();
+
+    // Selecciona el contenedor principal con el texto "Usuario"
+    const headerDataContainerUsuario = page.locator('div.header-data-container:has-text("Usuario")');
+    // Verifica que el contenedor sea visible
+    await expect(headerDataContainerUsuario).toBeVisible();
+    // Haz clic en el contenedor
+    await headerDataContainerUsuario.click();
+
+        // Selecciona el contenedor principal con el texto "Factura"
+        const headerDataContainerFactura = page.locator('div.header-data-container:has-text("Factura")');
+        // Verifica que el contenedor sea visible
+        await expect(headerDataContainerFactura).toBeVisible();
+        // Haz clic en el contenedor
+        await headerDataContainerFactura.click();
   });
 });
